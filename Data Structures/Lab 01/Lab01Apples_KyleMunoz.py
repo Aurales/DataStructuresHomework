e81b16318125eb7ecfdfac645748f38e84eb2ef6
Kyle Munoz <kdm2@graceland.edu>
2018-01-21 16:53:51 -0600

    Commiter

--- Data Structures/Lab 01/Lab01Apples_KyleMunoz.py
+++ Data Structures/Lab 01/Lab01Apples_KyleMunoz.py
@@ -1,17 +1,41 @@
 #Kyle Munoz
-#Make a program that ask for the chronological price of apples and calculates the best possible profit.
+#Make a program that ask for the chronological price of apples and calculates the best possible profit from a single purchase and sale of apples.
 def main():
-    apples = [14,6,9,7,8,10,3,9]
+    apples = []
     applesvalue = []
-##    while True:
-##        try:
-##            apples.append(int(input("Please enter the prices of apples one at a time: ")))
-##            print(apples)
-##        except ValueError:
-##            break
+    positiveapplevalue = []
+    #Collect all apple values
+    while True:
+        try:
+            apples.append(int(input("Please enter the prices of apples one at a time. If you have nothing left to enter press enter one more time: ")))
+        except ValueError:
+            break
+    #Get the length of the list 
     applelistlength = len(apples)
-    for apple in apples:
-        difference = int(apples[apple]) - int(apples[apple])
-        print(difference)
-   
-main()
+    #Compare the price difference from one purchase to one sale
+    for i in range(applelistlength):
+        try:
+            applesvalue.append((-1) * (apples[i] - apples[i+1]))
+        except IndexError:
+                break
+    #Add all poitive values to new list
+    for value in applesvalue:
+        if value > 0:
+            positiveapplevalue.append(value)
+    for i in range(100):
+        try:
+            if positiveapplevalue[0] < positiveapplevalue[1]:
+                del(positiveapplevalue[0])
+            elif positiveapplevalue[0] < positiveapplevalue[2]:
+                del(positiveapplevalue[0])
+            elif positiveapplevalue[0] < positiveapplevalue[3]:
+                del(positiveapplevalue[0])
+            elif positiveapplevalue[0] < positiveapplevalue[4]:
+                del(positiveapplevalue[0])
+            elif positiveapplevalue[0] < positiveapplevalue[5]:
+                del(positiveapplevalue[0])
+        except IndexError:
+            break
+    greatestapplevalue = positiveapplevalue[0] 
+    print("The greatest possible profit from the purchase and sale of apples is", greatestapplevalue, "units per apple.")
+main()  
