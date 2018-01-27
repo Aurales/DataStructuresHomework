7c20071bdfa56f4700b38251ea6d5ae97028bb36
Kyle Munoz <kdm2@graceland.edu>
2018-01-27 16:58:23 -0600

    Commiter

---
+++ Data Structures/Lab 02/Lab02PhysiciansOffice_KyleMunoz.py
@@ -0,0 +1,38 @@
+#Kyle Munoz
+#Physicians Office Simulation
+
+import random
+
+#Class Physician
+class Physician:
+    def __init__(self, name):
+        self.name = name
+        Helping = 0
+
+#Class Nurse
+class Nurse:
+    def __init__(self, name):
+        self.name = name
+        Helping = 0
+
+#Class Patient
+class Patient:
+    def __init__(self, name, waitingroom):
+        self.name = name
+        waitingroom.append(name)
+        self.DoctorHelpTime = int(15 + random.randint(0,6))
+        HasBeenHelped = 0
+
+#Waiting Room
+    #1 for entering patients
+    #2 for after visiting with a triage nurse
+Waitingroom1 = []
+Waitingroom2 = []
+
+#Exam Room
+#0 indicates and empty room
+ExamRooms = [0,0,0,0,0,0]
+ExamRoomPatients = ["noone","noone","noone","noone","noone","noone"]
+
+
+
